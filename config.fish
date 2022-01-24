ae86d474c27b876168fadd2cb9f765bf8d5b4aa6
Corazone777 <59613719+Corazone777@users.noreply.github.com>
2022-01-24 12:45:01 +0100

    Add files via upload

--- config.fish
+++ config.fish
@@ -2,18 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 	#Aliases
 	alias battery='cat /sys/class/power_supply/BAT1/capacity'
-	alias anime='~/github/ani-cli/ani-cli'
 	alias lock='xlock'
 	alias config='fish_config'
-	alias lyrics='swaglyrics -c'
-	alias lyrics_web='swaglyrics -t'
 	alias em='emacsclient'
+	alias startup='$HOME/scripts/startup.sh'
+	alias sicp='firefox --new-tab https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-4.html'
+	alias init 0='poweroff'
+	alias init 6='reboot' 
+	alias freebsd='neofetch --ascii_distro freebsd'
 	
 	#Paths
 	export PATH="$HOME/.emacs.d/bin:$PATH"
 	export PATH="$HOME/.cargo/bin:$PATH"
 	export PATH="/usr/bin/elixir:$PATH"
 	export PATH="/usr/bin/mix:$PATH"
+
 
 	#Shell settings
 	set fish_greeting
